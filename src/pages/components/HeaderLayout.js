3645c51a4871143cc06897620723c1b788fe0859
DefeParedes <federicoparedes20@gmail.com>
2018-12-13 18:40:07 -0300

    Boton_Cambiante

---
+++ src/pages/components/HeaderLayout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HeaderLayout = (props) => {
+    console.log(props);
+    return ( 
+        <div className="header">
+            {props.children}
+            <button onClick = {props.clickHandler }>un boton</button>
+            <button onClick = {props.clickHandler }>un boton2</button>
+            <button onClick = {props.clickHandler }>un boton3</button>
+            <div>
+                <h2>{props.texto_boton_2}</h2>
+            </div>
+            <button>{props.texto_boton_2}</button>
+        </div>
+     );
+}
+ 
+export default HeaderLayout;
